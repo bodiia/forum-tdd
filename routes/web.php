359379292f6f99c92dc08368d015f69eb894e3e2
Vadim0802 <v.bodyansky@yandex.ru>
2022-09-27 00:59:12 +1000

    some cosmetic fix

--- routes/web.php
+++ routes/web.php
@@ -8,21 +8,32 @@ use App\Http\Controllers\ReplyController;
 use App\Http\Controllers\ThreadController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomeController::class)->name('home');
+Route::get('/', HomeController::class)
+    ->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('threads', ThreadController::class)->except(['show', 'index', 'destroy']);
 
-    Route::post('threads/{channel}/{thread}/replies', [ReplyController::class, 'store'])->name('threads.replies.store');
-    Route::post('replies/{reply}/favorites', [FavoriteController::class, 'store'])->name('favorites.store');
-    Route::delete('threads/{channel}/{thread}', [ThreadController::class, 'destroy'])->name('threads.channel.destroy');
+    Route::post('threads/{channel}/{thread}/replies', [ReplyController::class, 'store'])
+        ->name('threads.replies.store');
+
+    Route::post('replies/{reply}/favorites', [FavoriteController::class, 'store'])
+        ->name('favorites.store');
+
+    Route::delete('threads/{channel}/{thread}', [ThreadController::class, 'destroy'])
+        ->name('threads.channel.destroy');
 });
 
-Route::get('threads', [ThreadController::class, 'index'])->name('threads.index');
+Route::get('threads', [ThreadController::class, 'index'])
+    ->name('threads.index');
 
-Route::get('threads/{channel}', [ChannelController::class, 'index'])->name('threads.channel.index');
-Route::get('threads/{channel}/{thread}', [ThreadController::class, 'show'])->name('threads.show');
+Route::get('threads/{channel}', [ChannelController::class, 'index'])
+    ->name('threads.channel.index');
 
-Route::get('profiles/{user}', [ProfileController::class, 'show'])->name('profiles.show');
+Route::get('threads/{channel}/{thread}', [ThreadController::class, 'show'])
+    ->name('threads.show');
+
+Route::get('profiles/{user}', [ProfileController::class, 'show'])
+    ->name('profiles.show');
 
 require __DIR__.'/auth.php';
