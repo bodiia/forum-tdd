9023bf243e2bc39f4c83da3bb27cd7c0c7ac0717
Vadim0802 <v.bodyansky@yandex.ru>
2022-09-29 09:04:02 +1000

    install vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,12 @@
 import './bootstrap';
-
+import { createApp } from 'vue/dist/vue.esm-bundler';
+import Flash from './components/Flash.vue';
 import Alpine from 'alpinejs';
 
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const app = createApp()
+    .component('flash', Flash)
+    .mount('#app');
