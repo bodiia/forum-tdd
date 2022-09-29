a52f1e796f25b43b0d4b013e2796e586cfcdede6
Vadim0802 <v.bodyansky@yandex.ru>
2022-09-29 20:32:19 +1000

    added icons for activity types

--- resources/views/profiles/activities/created_favorite.blade.php
+++ resources/views/profiles/activities/created_favorite.blade.php
@@ -1,7 +1,12 @@
 <x-slot name="header">
-    <a class="underline underline-offset-4" href="{{ route('threads.show', ['channel' => $record->subject->favorited->thread->channel, 'thread' => $record->subject->favorited->thread]) }}">
-        {{ $user->name }} favorited a reply
-    </a>
+    <div class="flex items-center gap-x-4">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
+        </svg>
+        <a class="underline underline-offset-4" href="{{ route('threads.show', ['channel' => $record->subject->favorited->thread->channel, 'thread' => $record->subject->favorited->thread]) }}">
+            {{ $user->name }} favorited a reply
+        </a>
+    </div>
 </x-slot>
 
 <x-slot name="body">
--- resources/views/profiles/activities/created_thread.blade.php
+++ resources/views/profiles/activities/created_thread.blade.php
@@ -1,5 +1,12 @@
 <x-slot name="header">
-    {{ $user->name }} published <a class="underline underline-offset-4" href="{{ route('threads.show', ['channel' => $record->subject->channel, 'thread' => $record->subject]) }}">{{ $record->subject->title }}</a>
+    <div class="flex items-center gap-x-4">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+        </svg>
+        <span>
+            {{ $user->name }} published <a class="underline underline-offset-4" href="{{ route('threads.show', ['channel' => $record->subject->channel, 'thread' => $record->subject]) }}">{{ $record->subject->title }}</a>
+        </span>
+    </div>
 </x-slot>
 
 <x-slot name="body">
