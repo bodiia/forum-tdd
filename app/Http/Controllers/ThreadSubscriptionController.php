2902b9c140d3aa4d3d7048f882daa60d669d2eaa
Vadim0802 <v.bodyansky@yandex.ru>
2022-12-18 14:25:09 +1000

    Remove unused imports

--- app/Http/Controllers/ThreadSubscriptionController.php
+++ app/Http/Controllers/ThreadSubscriptionController.php
@@ -5,11 +5,8 @@ namespace App\Http\Controllers;
 use App\Actions\ThreadSubscriptions\SubscribeUserAction;
 use App\Actions\ThreadSubscriptions\UnsubscribeUserAction;
 use App\Models\Thread;
-use App\Models\ThreadSubscription;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Redirect;
 
 class ThreadSubscriptionController extends Controller
